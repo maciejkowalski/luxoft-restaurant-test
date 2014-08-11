e0b73452f0d8423ca9225372e11bf4454d07b8f9
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-08-11 19:29:22 +0200

    refactor reservation spec

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -6,11 +6,20 @@ RSpec.describe Reservation, :type => :model do
   it { should validate_presence_of(:table_id) }
 
   describe 'validations' do
-    it 'checks availability' do
-      Reservation.create!(from_time: (Time.now - 2.hours), to_time: (Time.now - 1.hours))
+    let!(:table) { FactoryGirl.create(:table) }
+    let(:reservation_params) do
+      {
+        from_time: (Time.now - 2.hours),
+        to_time: (Time.now - 1.hours),
+        table_id: table.id
+      }
+    end
 
-      reservation = described_class.new(from_time: (Time.now - 2.hours), to_time: (Time.now - 1.hours))
-      expect(reservation.save).to eq(false)
+    it 'checks availability' do
+      Reservation.create!(reservation_params)
+
+      duplicated_reservation = described_class.new(reservation_params)
+      expect(duplicated_reservation.save).to eq(false)
     end
   end
 end
