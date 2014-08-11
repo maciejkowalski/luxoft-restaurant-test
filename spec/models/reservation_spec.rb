e76b12f7d4f3295f220d6bb1618b09b82d7a7fa6
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-08-11 18:17:29 +0200

    add simple test case for reservation time validation

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Reservation, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it 'checks availability' do
+      Reservation.create!(from_time: (Time.now - 2.hours), to_time: (Time.now - 1.hours))
+
+      reservation = described_class.new(from_time: (Time.now - 2.hours), to_time: (Time.now - 1.hours))
+      expect(reservation.save).to eq(false)
+    end
+  end
 end
