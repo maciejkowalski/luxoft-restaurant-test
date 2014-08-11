02b6bd129acf02a4782d13db4e1f03e4119b2c35
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-08-11 17:43:24 +0200

    update db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811152129) do
+ActiveRecord::Schema.define(version: 20140811153948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tables", force: true do |t|
+    t.integer  "number"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
