596030f804b9605dcbaa88217531bd34538e70ae
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-08-12 12:01:42 +0200

    Reservation: add validation logic for checking time availability

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,24 @@ class Reservation < ActiveRecord::Base
   belongs_to :table
 
   validates :table_id, presence: true
+  validate :check_availability
+
+  private
+
+  def check_availability
+    query = "NOT (
+      (:from_time <= reservations.from_time AND :from_time <= reservations.to_time AND
+      :to_time <= reservations.from_time AND :to_time <= reservations.to_time)
+      OR
+      (reservations.from_time <= :from_time AND reservations.to_time <= :from_time AND
+      reservations.from_time <= :to_time AND reservations.to_time <= :to_time)
+    )"
+
+    scope = Reservation.joins(:table).where(table_id: self.table_id)
+    scope = scope.where(query, {from_time: self.from_time, to_time: self.to_time})
+
+    if scope.count > 0
+      errors.add(:availability, "please select other time period, current is already reserved")
+    end
+  end
 end
